4fa2630e4390725312f378f88a1d742e1b6ad439
Sangin-C <sangin3435@naver.com>
2021-08-17 15:07:51 +0900

    섹션7. 의존관계 자동 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,14 +9,43 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    //final 을 붙이면 생성자에서만 값을 넣어줄수 있다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+/*
+    //생성자 주입은 하지 않는게 좋다!
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private DiscountPolicy discountPolicy;
+*/
+
+/*
+    //stter 주입보다 생성자 주입이 좋다!
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+*/
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+/*
+
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
