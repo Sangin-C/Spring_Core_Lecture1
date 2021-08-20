86545bb45720a223dcb1aa025fbd93edc592e190
Sangin-C <sangin3435@naver.com>
2021-08-21 00:50:40 +0900

    섹션9. 빈 스코프 마무리

--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -2,7 +2,6 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Service;
 public class LogDemoService {
 
     private final MyLogger myLogger;
+
     public void logic(String id) {
         myLogger.log("service id = " + id);
     }
